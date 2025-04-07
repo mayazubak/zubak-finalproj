7234a5d2d168add5fa4377d4220132f635a13a61
mayazubak <148928157+mayazubak@users.noreply.github.com>
2025-04-07 16:10:14 -0500

    Project Check in 2: readme updated, run_experiment updated with test file descriptions, exp.py updated with function outlines.

--- run_experiment.py
+++ run_experiment.py
@@ -1,10 +1,24 @@
 from src import exp
 from src import gui
 
+# test file description
+
 def main():
-    the_gui = gui.Gui()
-    the_experiment = exp.Exp(the_gui)
-    the_gui.root.mainloop()
+    
+    # test launches, programs creates an instance of the Test class
+    
+    # runs ask_initial_questions(), stores responses/score
+    
+    # program calls generate_digits() to print a sequence of numbers
+    # user tasked with repeating numbers back, stores responses/scores
+    
+    # program calls generate_shape(), user is asked to identify shape, 
+    # stores repsonse/score
+    
+    # program calls calculate_score() to calculate overall score based on
+    # stored information from other tasks, finals scoe then printed. 
+   
+       
 
 if __name__ == '__main__':
     main()
---
+++ src/exp.py
@@ -0,0 +1,23 @@
+
+class Test:
+    def __init__(self):
+        self.score = 0
+        
+    def ask_initial_questions(self):
+        
+        # asks preliminary cognitive function questions to asses
+        # user's basic orientation
+        # stores responses in a list
+        
+    def generate_digits(self, length = 7):
+        
+        # generates digits for digit span task of test
+        
+    def generate_shape(self):
+       
+        # returns image of shape for user to identify
+        
+    def calculate_score(self):
+        
+        # calculates scores based on all the questions in MMSE and 
+        # digit span tasks
